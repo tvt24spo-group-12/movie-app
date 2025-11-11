8629e3a6d7cf742b5ab253e295fd9900abe4961b
miikamenk <miikarito@gmail.com>
2025-11-11 21:56:43 +0200

    Database overhaul made movie_id into int to be able to match it to the ID got from TMDB
    changed name to title to reflect the field in TMDB api
    
    added a bunch of useful fields to the movie table (genres, overview, status, release_date, etc.)
    
    changed the last remaining camelCase names into snake_case.

--- database/database.sql
+++ database/database.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+-- create enum invitationstatus
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'enum_invitationstatus') THEN
@@ -10,51 +11,44 @@ BEGIN
     END IF;
 END$$;
 
-ALTER TABLE IF EXISTS public.favourite_movies DROP CONSTRAINT IF EXISTS movie_id;
-
-ALTER TABLE IF EXISTS public.favourite_movies DROP CONSTRAINT IF EXISTS user_id;
-
-ALTER TABLE IF EXISTS public.group_members DROP CONSTRAINT IF EXISTS fkey_group_id;
-
-ALTER TABLE IF EXISTS public.group_members DROP CONSTRAINT IF EXISTS fkey_user_id;
-
-ALTER TABLE IF EXISTS public.group_movie_suggestions DROP CONSTRAINT IF EXISTS fkey_group_id;
-
-ALTER TABLE IF EXISTS public.group_movie_suggestions DROP CONSTRAINT IF EXISTS fkey_movie_id;
-
-ALTER TABLE IF EXISTS public.movie_votes DROP CONSTRAINT IF EXISTS fkey_user_id;
-
-ALTER TABLE IF EXISTS public.movie_votes DROP CONSTRAINT IF EXISTS fkey_movie_id;
+-- create type genre
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'genre_type') THEN
+		CREATE TYPE genre_type AS (
+		  id   int,
+		  name text
+		);
+	END IF;
+END$$;
 
 
-
-DROP TABLE IF EXISTS public.groups;
 
 CREATE TABLE IF NOT EXISTS public.groups
 (
     group_id serial NOT NULL,
-    "groupName" character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
+    group_name character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
     owner_id integer NOT NULL,
-    users character varying(512)[] COLLATE pg_catalog."default" NOT NULL,
-    "userAmount" integer,
+    user_amount integer,
     CONSTRAINT "Groups_pkey" PRIMARY KEY (group_id)
 );
 
-DROP TABLE IF EXISTS public.movies;
-
 CREATE TABLE IF NOT EXISTS public.movies
 (
-    movie_id serial NOT NULL,
-    name text COLLATE pg_catalog."default" NOT NULL,
-    media_type text COLLATE pg_catalog."default",
-    "moviePicture" pg_snapshot,
+    movie_id integer NOT NULL,
+    title text COLLATE pg_catalog."default" NOT NULL,
+    original_title text,
+    overview text,
+    genres genre_type[] COLLATE pg_catalog."default",
+    status text,
+    release_date timestamp with time zone,
+    in_theaters boolean NOT NULL DEFAULT FALSE,
+    poster_path text,
+    backdrop_path text,
     vote_count integer,
     vote_average integer,
-    in_theaters boolean NOT NULL DEFAULT FALSE,
     CONSTRAINT "Movies_pkey" PRIMARY KEY (movie_id)
 );
-
-DROP TABLE IF EXISTS public.users;
 
 CREATE TABLE IF NOT EXISTS public.users
 (
@@ -62,12 +56,10 @@ CREATE TABLE IF NOT EXISTS public.users
     username character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
     password_hash character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
     email character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
-    "profilePicture" pg_snapshot,
+    picture_path text,
     "joinedGroups" integer[],
     CONSTRAINT "User_pkey" PRIMARY KEY (user_id)
 );
-
-DROP TABLE IF EXISTS public.favourite_movies;
 
 CREATE TABLE IF NOT EXISTS public.favourite_movies
 (
@@ -77,18 +69,14 @@ CREATE TABLE IF NOT EXISTS public.favourite_movies
     PRIMARY KEY (favorite_id)
 );
 
-DROP TABLE IF EXISTS public.group_members;
-
 CREATE TABLE IF NOT EXISTS public.group_members
 (
     member_id serial NOT NULL,
     group_id integer NOT NULL,
     user_id integer NOT NULL,
-    group_invitation enum_invitationstatus NOT NULL DEFAULT 'pending',
+    membership_status enum_invitationstatus NOT NULL DEFAULT 'pending',
     PRIMARY KEY (member_id)
 );
-
-DROP TABLE IF EXISTS public.group_movie_suggestions;
 
 CREATE TABLE IF NOT EXISTS public.group_movie_suggestions
 (
@@ -103,8 +91,6 @@ CREATE TABLE IF NOT EXISTS public.group_movie_suggestions
     PRIMARY KEY (suggestion_id)
 );
 
-DROP TABLE IF EXISTS public.movie_votes;
-
 CREATE TABLE IF NOT EXISTS public.movie_votes
 (
     vote_id serial NOT NULL,
@@ -114,6 +100,7 @@ CREATE TABLE IF NOT EXISTS public.movie_votes
     created_at timestamp with time zone DEFAULT NOW(),
     updated_at timestamp with time zone DEFAULT NOW(),
     review text,
+    public boolean NOT NULL DEFAULT FALSE,
     PRIMARY KEY (vote_id),
     CONSTRAINT "One per movie per user" UNIQUE (movie_id, user_id)
 );
