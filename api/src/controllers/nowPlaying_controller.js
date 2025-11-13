d2aac0d4ec58166302c87abb8fd68e1d82d8601c
ilphone <iluutzima@gmail.com>
2025-11-13 13:22:32 +0200

    Lisätty now in theaters api

---
+++ api/src/controllers/nowPlaying_controller.js
@@ -0,0 +1,11 @@
+import { fetchNowPlaying } from "../models/nowPlaying_model.js";
+
+export async function getNowPlaying(req, res) {
+  try {
+    const movies = await fetchNowPlaying();
+    res.json(movies);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch full now playing movies" });
+  }
+}
