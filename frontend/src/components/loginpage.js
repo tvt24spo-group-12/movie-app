d75da7b7c3c67b5ebaa5f4148325f22fb358cabc
Veeti-1 <veeti.salomaa155@gmail.com>
2025-11-17 14:40:06 +0200

    login/register-page ja sidebar css muotoilua parannettu

--- frontend/src/components/loginpage.js
+++ frontend/src/components/loginpage.js
@@ -2,10 +2,14 @@ import '../style/buttonStyle.css'
 import { useState } from 'react'
 import {signIn} from '../api/login'
 import '../style/sidebar.css'
+import RegisterPage from '../components/registerpage'
 
 export default function LoginPage(){
     const[Identifier, setIdentifier] = useState('')
     const[Password, setPassword] = useState('')
+
+    const[openRegister, setOpenRegister] = useState(true)
+    const[openLogin, setOpenLogin] = useState(true)
   const handleSubmit= async (e) =>{
         e.preventDefault();
 
@@ -23,6 +27,10 @@ export default function LoginPage(){
     }
 
 return(
+    <>
+    {openLogin &&(
+     < div className='page popupcontainer'>
+        
     <form onSubmit={handleSubmit}>
             <input
             className='placeHolder'
@@ -38,7 +46,26 @@ return(
             value={Password}
             onChange={e => setPassword(e.target.value)}
             />
+            <button
+            onClick={() => {
+                setOpenRegister(false)
+                setOpenLogin(false)
+                location.reload(false)
+            }}
+             className='btn-primary submitBtn cancelBtn'>
+            X
+        </button>
+            <button onClick={() =>{
+                setOpenRegister(true)
+                setOpenLogin(false)
+        }} className='goToRegister'>Don't have an account? Register Here!</button>
         <button className="btn-primary submitBtn">Log in</button>
         </form>
-)
+          </div>
+         )}
+         {openRegister && !openLogin && (<RegisterPage/>)}
+      
+       </>
+)   
 }
+//       
