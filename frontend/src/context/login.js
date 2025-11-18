4edcf69b1d34b382819c38717c568ad92d3772f5
Veeti-1 <veeti.salomaa155@gmail.com>
2025-11-18 13:01:23 +0200

    tallentaa user_id,username ja email ja lisäsin pikkutarkistukse useeffectissa tallentuiko

--- frontend/src/context/login.js
+++ frontend/src/context/login.js
@@ -9,7 +9,11 @@ export function AuthProvider({children}){
     const [accessToken, setAccessToken] = useState(null);
   //  const [loading, setLoading] = useState(true)
 
-  
+  useEffect(()=>{ //näkee tallentuiko muutokset ja (setUser & setAccessToken)
+    console.log("use: ",accessToken)
+    console.log("user: ",user)
+
+  },[accessToken,user])
     const signIn = async ( Identifier, Password) =>{
          const res = await fetch(url+'/signin', 
            { method: "POST",
@@ -26,8 +30,14 @@ export function AuthProvider({children}){
         throw new Error(error.error)
     }
     const data = await res.json()
-    setUser({Identifier : data.identifier});
+    setUser({
+      id: data.user_id,
+      email: data.email,
+      username: data.username
+    });
+    
     setAccessToken(data.token)
+   
     return data
 }
 const value = {
