56896a1a98ce59d8157596f262c94bfb5cbdc6f9
Veeti-1 <veeti.salomaa155@gmail.com>
2025-11-25 12:37:56 +0200

    lisätty favorite apia

--- api/src/controllers/favoriteMovies_controller.js
+++ api/src/controllers/favoriteMovies_controller.js
@@ -1,10 +1,9 @@
-import {addFavoriteMovie} from "../api/src/models/favoriteMovie_model";
+import {addFavoriteMovie} from "../models/favoriteMovie_model.js"
 
 const addFavorite = async (req, res) =>{
     try{
-        const{user} = req.body
-            const result = await addFavoriteMovie(user.favorite_id,user.user_id, user.movie_id)
-            res.json(result);
+            const result = await addFavoriteMovie(user_id, movie_id)
+            res.json({insertId: result.insertId, });
     }catch(error){
         res.status(500).json({error : "something went wrong"})
     }
--- api/src/models/favoriteMovie_model.js
+++ api/src/models/favoriteMovie_model.js
@@ -1,12 +1,13 @@
 
 import pool from '../database.js'
 
-const addFavoriteMovie = async (favorite_id,user_id, movie_id) =>{
+const addFavoriteMovie = async (user_id, movie_id) =>{
 
     try{
-        const res = await pool.query('INSERT INTO favourite_movies (favorite_id, user_id, movie_id) VALUES (?,?,?)',
-            [favorite_id,user_id,movie_id])
-            return res
+    
+        const [res] = await pool.query('INSERT INTO favourite_movies (user_id, movie_id) VALUES ($1,$2) RETURNING *',
+            [user_id,movie_id])
+            return res;
     }catch(error){
         throw new Error(error)
     }
