5ddb957c724771cb82e23c59c8e9690342a3b8e0
miikamenk <miikarito@gmail.com>
2025-12-11 19:58:22 +0200

    added more stuff to homepage and added links to users reviews

--- api/src/controllers/movie_controller.js
+++ api/src/controllers/movie_controller.js
@@ -1,5 +1,10 @@
 import { getAll } from "../models/movie_model.js";
-import { getMovieDetailsByID } from "../models/tmdb_model.js";
+import {
+  getMovieDetailsByID,
+  fetchPopular,
+  fetchTrending,
+  fetchUpcoming,
+} from "../models/tmdb_model.js";
 
 export async function getMovies(req, res, next) {
   try {
@@ -17,5 +22,35 @@ export async function getTMDBDetailsById(req, res, next) {
     res.json(data);
   } catch (err) {
     next(err);
+  }
+}
+
+export async function getPopular(req, res) {
+  try {
+    const movies = await fetchPopular();
+    res.json(movies);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch popular movies" });
+  }
+}
+
+export async function getTrending(req, res) {
+  try {
+    const movies = await fetchTrending();
+    res.json(movies);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch popular movies" });
+  }
+}
+
+export async function getUpcoming(req, res) {
+  try {
+    const movies = await fetchUpcoming();
+    res.json(movies);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch popular movies" });
   }
 }
